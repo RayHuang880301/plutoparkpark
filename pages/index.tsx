532b8613e565ee2af9d1a61220fb95914e1a0643
RayHuang <ray880301@gmail.com>
2022-07-13 19:02:10 +0800

    add font family

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ const Home: NextPage = () => {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;600;800&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@200;400;600;900&display=swap" rel="stylesheet"></link>
     </Head>
     <FrontCover />
     </>
