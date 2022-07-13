c80611bbcace5f22b14561ef7d8da9ce662722cf
abner <fantasrulai@gmail.com>
2022-07-13 21:38:30 +0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.scss'
-import '../styles/effect.scss'
 import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import '../styles/effect.scss';
+import '../styles/nft.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
