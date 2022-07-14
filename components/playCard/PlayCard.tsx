08219092d713df1bc852d63baf832c0ebe5e11e1
abner <fantasrulai@gmail.com>
2022-07-15 03:01:27 +0800

    fix effect

--- components/playCard/PlayCard.tsx
+++ components/playCard/PlayCard.tsx
@@ -29,113 +29,10 @@ export default function PlayCard(props: Props) {
   );
   // PLUTOEFFECT
   const root = useRef<HTMLDivElement>(null);
-  const remove = (ele: Element, time: number) => {
-      root.current?.append(ele);
-      setTimeout(() => {
-          ele.innerHTML = '';
-          ele.remove();
-      }, time);
-  }
-  function bounceInTop(name = FortuneType.Study, time = 700) {
-      const ele = createElement('ebit' + name, 'effect-item ef-bounce-in-top');
-      const img = createElement(`${name}-img`, '', 'img');
-      img.setAttribute('src', require(name));
-      ele.append(img);
-      if (Math.random() > 0.5) {
-          // img.style.transform = 'rotate(90deg)';
-          ele.style.bottom = `${(Math.random() * 7 + 1) / 10 * window.innerHeight}px`;
-          ele.style.left = '-20%';
-          ele.style.opacity = '0.2';
-          setTimeout(() => {
-              ele.style.left = '120%';
-              ele.style.opacity = '1';
-          }, 50);
-      } else {
-          ele.style.bottom = '115%';
-          ele.style.left = `${(Math.random() * 7 + 1) / 10 * window.innerWidth}px`;
-          ele.style.opacity = '0.2';
-          setTimeout(() => {
-              ele.style.bottom = '-20%';
-              ele.style.opacity = '1';
-          }, 50);
-      }
-      remove(ele, time);
-  }
-  function heartbeat(name = FortuneType.Study, time = 700) {
-      const ele = createElement('eh' + name, 'effect-item ef-heartbeat');
-      const img = createElement(`${name}-img`, '', 'img');
-      img.setAttribute('src', require(name));
-      ele.append(img);
-      const { x, y } = getRandomPosition();
-      ele.style.left = `${x}px`;
-      ele.style.top = `${y}px`;
-
-      remove(ele, time);
-  }
-  function waterfall(name = FortuneType.Study, time = 1500) {
-      let offset = 0;
-      const num = 6;
-      const offsetBase = Math.round(window.innerWidth / num);
-      new Array(num).fill(0).forEach((_, i) => {
-          const ele = createElement('tbp' + name, 'effect-item ef-waterfall top-bottom');
-          ele.style.transform = `translateX(${offset}px)`;
-          const box = createElement('tbpb' + name, 'box');
-          setTimeout(() => {
-              const img = createElement(`${name}-img-${i}`, '', 'img');
-              img.setAttribute('src', require(name));
-              box.append(img);
-              ele.append(box);
-
-              remove(ele, time);
-          }, (Math.random() * 30) * 3 * i);
-          offset += offsetBase;
-      });
-  }
-  function rotateRandom(name = FortuneType.Study, time = 500) {
-      const ele = createElement('eri' + name, 'effect-item ef-rotate-item');
-      const img = createElement(`${name}-img`, '', 'img');
-      img.setAttribute('src', require(name));
-      ele.append(img);
-      const { x, y } = getRandomPosition();
-      ele.style.left = `${x}px`;
-      ele.style.top = `${y}px`;
-
-      remove(ele, time);
-  }
-  function rotateRandomBatch(name = FortuneType.Study, time = 500) {
-      new Array(Math.round((Math.random() * 3) + 3)).fill(0).forEach(() => {
-          rotateRandom(name, time);
-      });
-  }
-  const fadeInCenterBig = (name = FortuneType.Study, time = 500) => {
-      const ele = createElement('efb' + name, 'effect-item ef-fade-big');
-      const img = createElement(`${name}-img`, '', 'img');
-      img.setAttribute('src', require(name));
-      ele.append(img);
-
-      remove(ele, time);
-  }
-
-  function animateFadeIn(name = FortuneType.Study, time = 1000) {
-      const ele = createElement('efaa' + name, 'effect-item ef-fade-in');
-      const img = createElement(`${name}-img`, '', 'img');
-      img.setAttribute('src', require(name));
-      ele.append(img);
-      const { x, y } = getRandomPosition(200);
-      ele.style.left = `${x}px`;
-      ele.style.top = `${y}px`;
-
-      remove(ele, time);
-  }
-  function animateFadeInBatch(name = FortuneType.Study, time = 1000) {
-      new Array(Math.round((Math.random() * 5) + 3)).fill(0).forEach(() => {
-          animateFadeIn(name, time);
-      });
-  }
   //
   
   const playRandomEffect = () => {
-    const effectList = [animateFadeInBatch, fadeInCenterBig, rotateRandomBatch, heartbeat ]
+    const effectList = [rotateRandomBatch, rotateRandomBatch, rotateRandomBatch, rotateRandomBatch ]
     const list = [effectMusic1, effectMusic2, effectMusic3, effectMusic4]
     const random = Math.floor(Math.random() * list.length);
     const player = list[random].current;
@@ -146,7 +43,7 @@ export default function PlayCard(props: Props) {
       }
       player.play();
       console.log(fortuneType)
-      effectList[random](fortuneType as any);
+      effectList[random](root.current, fortuneType as any);
     }
   };
 
@@ -183,6 +80,112 @@ export default function PlayCard(props: Props) {
     </div>
     </>
   )
+}
+
+function remove(current: Element | null, ele: Element, time: number) {
+  if(!current) return;
+  current.append(ele);
+  setTimeout(() => {
+      ele.innerHTML = '';
+      ele.remove();
+  }, time);
+}
+
+function bounceInTop(current: Element | null, name = FortuneType.Study, time = 700) {
+  const ele = createElement('ebit' + name, 'effect-item ef-bounce-in-top');
+  const img = createElement(`${name}-img`, '', 'img');
+  img.setAttribute('src', require(name));
+  ele.append(img);
+  if (Math.random() > 0.5) {
+      // img.style.transform = 'rotate(90deg)';
+      ele.style.bottom = `${(Math.random() * 7 + 1) / 10 * window.innerHeight}px`;
+      ele.style.left = '-20%';
+      ele.style.opacity = '0.2';
+      setTimeout(() => {
+          ele.style.left = '120%';
+          ele.style.opacity = '1';
+      }, 50);
+  } else {
+      ele.style.bottom = '115%';
+      ele.style.left = `${(Math.random() * 7 + 1) / 10 * window.innerWidth}px`;
+      ele.style.opacity = '0.2';
+      setTimeout(() => {
+          ele.style.bottom = '-20%';
+          ele.style.opacity = '1';
+      }, 50);
+  }
+  remove(current, ele, time);
+}
+function heartbeat(current: Element | null, name = FortuneType.Study, time = 700) {
+  const ele = createElement('eh' + name, 'effect-item ef-heartbeat');
+  const img = createElement(`${name}-img`, '', 'img');
+  img.setAttribute('src', require(name));
+  ele.append(img);
+  const { x, y } = getRandomPosition();
+  ele.style.left = `${x}px`;
+  ele.style.top = `${y}px`;
+
+  remove(current, ele, time);
+}
+function waterfall(current: Element | null, name = FortuneType.Study, time = 1500) {
+  let offset = 0;
+  const num = 6;
+  const offsetBase = Math.round(window.innerWidth / num);
+  new Array(num).fill(0).forEach((_, i) => {
+      const ele = createElement('tbp' + name, 'effect-item ef-waterfall top-bottom');
+      ele.style.transform = `translateX(${offset}px)`;
+      const box = createElement('tbpb' + name, 'box');
+      setTimeout(() => {
+          const img = createElement(`${name}-img-${i}`, '', 'img');
+          img.setAttribute('src', require(name));
+          box.append(img);
+          ele.append(box);
+
+          remove(current, ele, time);
+      }, (Math.random() * 30) * 3 * i);
+      offset += offsetBase;
+  });
+}
+function rotateRandom(current: Element | null, name = FortuneType.Study, time = 500) {
+  const ele = createElement('eri' + name, 'effect-item ef-rotate-item');
+  const img = createElement(`${name}-img`, '', 'img');
+  img.setAttribute('src', require(name));
+  ele.append(img);
+  const { x, y } = getRandomPosition();
+  ele.style.left = `${x}px`;
+  ele.style.top = `${y}px`;
+
+  remove(current, ele, time);
+}
+function rotateRandomBatch(current: Element | null, name = FortuneType.Study, time = 50000) {
+  new Array(Math.round((Math.random() * 3) + 3)).fill(0).forEach(() => {
+      rotateRandom(current, name, time);
+  });
+}
+const fadeInCenterBig = (current: Element | null, name = FortuneType.Study, time = 500) => {
+  const ele = createElement('efb' + name, 'effect-item ef-fade-big');
+  const img = createElement(`${name}-img`, '', 'img');
+  img.setAttribute('src', require(name));
+  ele.append(img);
+
+  remove(current, ele, time);
+}
+
+function animateFadeIn(current: Element | null, name = FortuneType.Study, time = 1000) {
+  const ele = createElement('efaa' + name, 'effect-item ef-fade-in');
+  const img = createElement(`${name}-img`, '', 'img');
+  img.setAttribute('src', require(name));
+  ele.append(img);
+  const { x, y } = getRandomPosition(200);
+  ele.style.left = `${x}px`;
+  ele.style.top = `${y}px`;
+
+  remove(current, ele, time);
+}
+function animateFadeInBatch(current: Element | null, name = FortuneType.Study, time = 1000) {
+  new Array(Math.round((Math.random() * 5) + 3)).fill(0).forEach(() => {
+      animateFadeIn(current, name, time);
+  });
 }
 
 function require(name: FortuneType) {
